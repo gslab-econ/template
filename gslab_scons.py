eb53142274e8f04532996ed0369b794b30257102
Shun-Yang <yangf2@carleton.edu>
2016-08-19 22:33:57 -0700

    fix linux

--- gslab_scons.py
+++ gslab_scons.py
@@ -1,5 +1,6 @@
 import os, sys, shutil, subprocess
-from gslab_fill.py.tablefill import tablefill
+from sys import platform
+from gslab_fill.tablefill import tablefill
 
 def build_tables(target, source, env):
     tablefill(input    = ' '.join(env.GetBuildPath(env['INPUTS'])), 
@@ -30,7 +31,7 @@ def build_stata(target, source, env, executable = 'StataMP'):
     target_file = str(target[0])
     target_dir  = os.path.dirname(target_file)
   
-    if os.name == 'posix':
+    if platform == "darwin":
       log_file  = target_dir + '/sconscript.log'
       loc_log   = os.path.basename(source_file).replace('.do','.log')
       if executable == 'StataSE':
@@ -45,6 +46,23 @@ def build_stata(target, source, env, executable = 'StataMP'):
             subprocess.check_output('statase -e ' + source_file, shell=True)      
           except subprocess.CalledProcessError:     
             subprocess.check_output('stata -e ' + source_file, shell=True)      
+      shutil.move(loc_log, log_file)
+
+    if platform == "linux" or platform == "linux2":
+      log_file  = target_dir + '/sconscript.log'
+      loc_log   = os.path.basename(source_file).replace('.do','.log')
+      if executable == 'StataSE':
+        os.system('statase -b %s ' % source_file)        
+      elif executable == 'Stata':
+        os.system('stata -b %s ' % source_file)
+      elif executable == 'StataMP': 
+        try:
+          subprocess.check_output('statamp -b ' + source_file, shell=True)
+        except subprocess.CalledProcessError:       
+          try: 
+            subprocess.check_output('statase -b ' + source_file, shell=True)      
+          except subprocess.CalledProcessError:     
+            subprocess.check_output('stata -b ' + source_file, shell=True)      
       shutil.move(loc_log, log_file)
 
     return None
