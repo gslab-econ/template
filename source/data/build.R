88a032889f258e312925de0299df54e711900e86
Levi Boxell <leviboxell@gmail.com>
2016-08-26 12:12:43 -0700

    Fixes source/data

--- source/data/build.R
+++ source/data/build.R
@@ -1,6 +1,6 @@
 main <- function(){
     x    <- seq(-10, 10, 0.1)
-    write.table(x, "output/data/data.txt", sep = "|", row.names = FALSE, col.names = FALSE) 
+    write.table(x, "build/data/data.txt", sep = "|", row.names = FALSE, col.names = FALSE) 
 }
 
 main()
