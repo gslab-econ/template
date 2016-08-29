3fe5c445abb8dca84e38888df7b9db185f0a5dc3
M-R-Sullivan <m.r.sullivan@stanford.edu>
2016-08-29 15:44:25 -0700

    test2

--- source/data/build.R
+++ source/data/build.R
@@ -1,5 +1,5 @@
 main <- function(){
-    x    <- seq(-100, 100, 0.1)
+    x    <- seq(-13, 100, 0.1)
     write.table(x, "build/data/data.txt", sep = "|", row.names = FALSE, col.names = FALSE) 
 }
 
