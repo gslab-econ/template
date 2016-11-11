b736c86acd4ae6c8fc7ead68a0de8b11ea868676
chuanyu <yuchuan2016@gmail.com>
2016-11-11 12:22:47 -0800

    #20 Modify build.R

--- source/data/build.R
+++ source/data/build.R
@@ -1,6 +1,2 @@
-main <- function(){
-    x    <- seq(-13, 100, 0.1)
-    write.table(x, "build/data/data.txt", sep = "|", row.names = FALSE, col.names = FALSE) 
-}
-
-main()
+x <- seq(-13, 100, 0.1)
+write.table(x, "build/data/data.txt", sep = "|", row.names = FALSE, col.names = FALSE)
