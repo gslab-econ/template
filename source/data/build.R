218ffb652cda79908cfa8293681761edef34a7db
Levi Boxell <leviboxell@gmail.com>
2017-01-26 12:02:07 -0800

    #28 Fixes spacing

--- source/data/build.R
+++ source/data/build.R
@@ -2,7 +2,7 @@ args = commandArgs(TRUE)
 main <- function(i){
     x    <- seq(-10, 10, 0.1)
     write.table(x, sprintf("build/data/%s.txt", i), sep = "|", 
-    				row.names = FALSE, col.names = TRUE, quote = FALSE) 
+                   row.names = FALSE, col.names = TRUE, quote = FALSE) 
 }
 
 main(args[1])
