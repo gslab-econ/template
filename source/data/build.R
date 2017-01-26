895f2dc73cce1f44caed98c044ffdc8946e76c90
Levi Boxell <leviboxell@gmail.com>
2017-01-26 12:05:06 -0800

    Pull request for #28: Command Line argument example (#31)
    * gslab-econ/gslab-python#26 Demonstrates using CL args
    
    * More edits/testing
    
    * Removes gslab_scons1 and edits SConstruct
    
    * #28 Implements PR
    
    * gslab_python#26 Removes print statement
    
    * #28 Fixes spacing

--- source/data/build.R
+++ source/data/build.R
@@ -1,3 +1,8 @@
-set.seed(1)
-x <- sample(1:10, 1000, replace = T)
-write.table(x, "build/data/data.txt", sep = "|", row.names = FALSE, col.names = TRUE, quote = FALSE)
+args = commandArgs(TRUE)
+main <- function(i){
+    x    <- seq(-10, 10, 0.1)
+    write.table(x, sprintf("build/data/%s.txt", i), sep = "|", 
+                   row.names = FALSE, col.names = TRUE, quote = FALSE) 
+}
+
+main(args[1])
