22196f15b99652c57f6c0705f256df14cd37f166
Quan <lequan@stanford.edu>
2017-02-27 15:52:11 -0800

    Pull request for gslab_python/#57: Fix stata bug in Windows (#37)
    * gslab_python/#57 refactor check_stata()
    
    * #57 split commands in check_stata_packages()
    
    * gslab_python/issue57-winstata allow stata_flavor to be empty in the yaml file
    
    * gslab_python/issue57-winstata fix percent sign error
    
    * #37 Commits git-lfs

--- setup.py
+++ setup.py
@@ -17,7 +17,9 @@ def setup_test(ARGUMENTS):
     import yaml
     user_configs = yaml.load(open("user-config.yaml", 'rU'))
     mode         = ARGUMENTS.get('mode', 'develop') # Gets mode; defaults to 'develop'
-    sf           = ARGUMENTS.get('sf', user_configs['stata_flavor']) # Gets user supplied stata from command line or yaml
+    # Gets user supplied stata from command line or yaml
+    sf_configs   = user_configs.get('stata_flavor', None) 
+    sf           = ARGUMENTS.get('sf', sf_configs) 
     cache_dir    = user_configs['cache']
     if mode in ['cache', 'release']:
         check_cache(cache)
@@ -96,36 +98,27 @@ def check_cache(cache):
 
 def check_stata(sf):
     import gslab_scons.misc as misc
-    command = ''
-    flavors = ['stata-mp', 'stata-se', 'stata']
-    if sf is not None:
-        flavors = [sf] + flavors
-    if misc.is_unix():
-        for flavor in flavors:
-            if misc.is_in_path(flavor):
-                command = misc.stata_command_unix(flavor)
-                break
-    elif sys.platform == 'win32':
-        try:
-            key_exist = os.environ['STATAEXE'] is not None
-            command   = misc.stata_command_win("%%STATAEXE%%")
-        except KeyError:
-            flavors = [(f.replace('-', '') + '.exe') for f in flavors]
-            if misc.is_64_windows():
-                flavors = [f.replace('.exe', '-64.exe') for f in flavors]
-            for flavor in flavors:
-                if misc.is_in_path(flavor):
-                    command = misc.stata_command_win(flavor)
-                    break        
-    if command == '':
-        raise PrerequisiteError('Stata is not installed or excecutable is not added to path')
 
+    # Fake scons-like env dict for misc.get_stata_executable(env)
+    fake_env = {'user_flavor': sf} 
+    stata_exec = misc.get_stata_executable(fake_env)
+    
+    if stata_exec is None:
+        raise PrerequisiteError('Stata is not installed or executable is not added to path')
+    
+    command = misc.get_stata_command(stata_exec)
     check_stata_packages(command)
 
 def check_stata_packages(command):
-    command = command.split("%s")[0]
+    import gslab_scons.misc as misc
+    if misc.is_unix():
+        command = command.split("%s")[0]
+    elif sys.platform == "win32":
+        command = command.split("do")[0]
+
     for pkg in ['yaml']:
-        subprocess.check_output(command + '"which %s"' % pkg , shell = True) # http://www.stata.com/statalist/archive/2009-12/msg00493.html and http://stackoverflow.com/questions/18962785/oserror-errno-2-no-such-file-or-directory-while-using-python-subprocess-in-dj
+        command = (command + "which %s") % pkg
+        subprocess.check_output(command, stderr = subprocess.STDOUT, shell = True) # http://www.stata.com/statalist/archive/2009-12/msg00493.html and http://stackoverflow.com/questions/18962785/oserror-errno-2-no-such-file-or-directory-while-using-python-subprocess-in-dj
         with open('stata.log', 'rU') as f:
             log = f.read()
         if re.search('command %s not found' % pkg, log):
