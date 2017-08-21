03fafac944f12d776e14a04b7460b2db06a58aca
chuanyu <yuchuan2016@gmail.com>
2017-08-21 14:48:17 -0700

    #62 allow provenance to check missing files

--- paper_slides/print_provenance.py
+++ paper_slides/print_provenance.py
@@ -3,12 +3,14 @@ import os
 import subprocess
 
 def print_provenance():
+    PATHS = yaml.load(open('constants.yaml', 'rU'))
     header = 'Note: `Provenance.txt` is produced by `print_provenance.py`. Do not directly edit this file.\n\n'
     inputs = load_inputs()
-    with open('input/provenance.txt', 'wb') as provenance:
+    check_missing_inputs(PATHS, inputs)
+    with open('%s/provenance.txt' % PATHS['input'], 'wb') as provenance:
         provenance.write(header)
         for f in inputs.keys():
-            if not os.path.isfile('input/%s' % f):
+            if not os.path.isfile('%s/%s' % (PATHS['input'], f)):
                 raise IOError('%s does not exist.' % f)
             args   = ['git', '--no-pager', 'log', '-1', '--date=format:%Y-%m-%d %H:%M:%S',
                     '--format=%h at %cd', 'input/%s' % f]
@@ -23,3 +25,8 @@ def load_inputs():
     inputs = {'plot.eps' : '../analysis/%s/' % PATHS['build']['analysis'],
               'table.txt': '../analysis/%s/' % PATHS['build']['analysis']}
     return inputs
+
+def check_missing_inputs(PATHS, inputs):
+    for f in os.listdir('%s' % PATHS['input']):
+        if f not in ['.DS_Store', 'provenance.txt'] + inputs.keys():
+            raise IOError('The provenance of %s is not specified.' % f)
