1a53f379f833c396a68f15342bf18cf2b66b217f
Adam Rosenberg <arosenbe@stanford.edu>
2017-08-31 16:45:53 -0700

    83 gslab-econ/gslab_python#99 symlink to scons.py

--- paper_slides/scons.py
+++ paper_slides/scons.py
@@ -1,15 +1,1 @@
-import sys
-import subprocess
-
-def main():
-    # Pass on command line args
-    del sys.argv[0]
-    cl_args = ' '.join(sys.argv)
-    
-    # Create call
-    call = 'python ../config/scons/scons.py %s' % cl_args
-    
-    # Execute
-    subprocess.call(call, shell = True)
-
-main()
+../config/scons/scons.py
