deec354f2947c8ea617c9e58229b7c1cad2e7b70
Adam Rosenberg <arosenbe@stanford.edu>
2017-09-06 16:25:40 -0700

    #86 incorporate external logging from gslab-econ/gslab_python#131

--- analysis/source/prepare_data/create_data.r
+++ analysis/source/prepare_data/create_data.r
@@ -1,9 +1,9 @@
 library(yaml)
-PATHS <- yaml.load_file("config_global.yaml")
+paths_global <- yaml.load_file("config_global.yaml")
 
 main <- function() {
     x <- 1:300000
-    write.table(x, sprintf("%s/data.txt", PATHS$build$prepare_data),
+    write.table(x, sprintf("%s/data.txt", paths_global$build$prepare_data),
                 row.names = FALSE, col.names = TRUE, quote = FALSE)
 }
 main()
