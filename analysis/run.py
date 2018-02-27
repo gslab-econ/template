c2b8b28a994e246595fe75c5c109ec9ddaa86053
Zane Kashner <zanekashner@gmail.com>
2018-02-27 03:37:39 -0800

    practice task

--- analysis/run.py
+++ analysis/run.py
@@ -1,20 +1,20 @@
 import sys
 import subprocess
 
-execute_script = '../config/scons/scons.py'
+execute_script = 'source/prepare_data/create_data.r'
 
 def main(execute_script):
     '''
     Execute scons-local script.
-    Pass on command line arguments from this script's execution. 
+    Pass on command line arguments from this script's execution.
     '''
     # Pass on command line args
     del sys.argv[0]
     cl_args = ' '.join(sys.argv)
-    
+
     # Create call
-    call = 'python %s %s' % (execute_script, cl_args)
-    
+    call = 'Rscript %s %s' % (execute_script, cl_args)
+
     # Execute
     subprocess.call(call, shell = True)
 
