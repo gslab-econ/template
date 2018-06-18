ee95b9c961a9885eb45af3021c09d6ae453c7e00
Quan <lequan@stanford.edu>
2018-06-18 16:04:46 -0700

    #106 switch to using requirements.txt

--- config/config_python.py
+++ config/config_python.py
@@ -1,21 +1,17 @@
 import pip
-import pkg_resources
-# *** Add required packages to this list ***
-packages = ['pyyaml', 'numpy', 'matplotlib']
-# *** Add required packages to this list ***
+import sys
+import subprocess
 
-def main(packages, upgrade = False):
+
+def main(filename='config/requirements.txt', upgrade=False):
     # Install gslab_tools if it's not installed yet or it has the wrong version
-    pip.main(['install', '--upgrade', 'git+http://git@github.com/gslab-econ/gslab_python.git@v4.1.0'])
+    command = [sys.executable, '-m', 'pip', 'install', '-r', filename]
+    if upgrade:
+        command.append('--upgrade')
+    subprocess.check_call(command)
+    return None
 
-    # Install required packages using pip
-    installed_packages = [pkg.key for pkg in pip.get_installed_distributions()]
-    for pkg in packages:
-        if upgrade:
-            pip.main(['install', '--upgrade', pkg])
-        elif pkg not in installed_packages:
-            pip.main(['install', pkg])
 
 # upgrade = TRUE will update all packages to the most current version
-# upgrade = FALSE will skip packages that are already installed
-main(packages, upgrade = False)
+# The default option will skip packages that are already installed
+main()
