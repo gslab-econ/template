ee45fd02a953ac0217922b1d9e0811cf0ac49485
David Ritzwoller <David.Ritzwoller@gmail.com>
2018-07-06 16:25:48 -0700

    #106 change the installation of gslab python

--- config/config_python.py
+++ config/config_python.py
@@ -4,14 +4,19 @@ import subprocess
 
 
 def main(filename='config/requirements.txt', upgrade=False):
-    # Install gslab_tools if it's not installed yet or it has the wrong version
+    
+    # Install base python requirements, if they are not installed or are the wrong version
     command = [sys.executable, '-m', 'pip', 'install', '-r', filename]
     if upgrade:
         command.append('--upgrade')
     subprocess.check_call(command)
     return None
 
+    # Install gslab_tools
+    subprocess.call(['pip', 'install', "git+https://git@github.com/gslab-econ/gslab_python.git@master"])
 
 # upgrade = TRUE will update all packages to the most current version
 # The default option will skip packages that are already installed
 main()
+
+
