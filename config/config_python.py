25598ee38d82bc0c10f78d1e005bb9549d993a2c
David Ritzwoller <David.Ritzwoller@gmail.com>
2018-07-06 17:02:27 -0700

    #106 fixed typo

--- config/config_python.py
+++ config/config_python.py
@@ -5,15 +5,15 @@ import subprocess
 
 def main(filename='config/requirements.txt', upgrade=False):
     
+    # Install gslab_tools
+    subprocess.call(['pip', 'install', "git+https://git@github.com/gslab-econ/gslab_python.git@master"])
+    
     # Install base python requirements, if they are not installed or are the wrong version
     command = [sys.executable, '-m', 'pip', 'install', '-r', filename]
     if upgrade:
         command.append('--upgrade')
     subprocess.check_call(command)
     return None
-
-    # Install gslab_tools
-    subprocess.call(['pip', 'install', "git+https://git@github.com/gslab-econ/gslab_python.git@master"])
 
 # upgrade = TRUE will update all packages to the most current version
 # The default option will skip packages that are already installed
