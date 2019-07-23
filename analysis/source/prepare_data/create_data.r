2857a9b27b90882f84be43f1526c483f25d62191
Molly Wharton <mollywharton@siepr-mg-ra-imac-5.stanford.edu>
2019-07-23 09:49:55 -0700

    #258 informative message

--- analysis/source/prepare_data/create_data.r
+++ analysis/source/prepare_data/create_data.r
@@ -1,9 +1,11 @@
 library(yaml)
 CONFIG <- yaml.load_file("config_global.yaml")
+build_path <- CONFIG$build$prepare_data
+
 
 main <- function() {
     x <- 1:300000
-    write.table(x, sprintf("%s/data.txt", CONFIG$build$prepare_data),
+    write.table(x, sprintf("%s/data.txt", build_path),
                 row.names = FALSE, col.names = TRUE, quote = FALSE)
 }
 main()
