1c9a07e60f2c007faa9208087ad9f6b781c787df
David Ritzwoller <David.Ritzwoller@gmail.com>
2019-07-23 13:19:40 -0700

    Revert errant commit
    This reverts commit 2857a9b27b90882f84be43f1526c483f25d62191.

--- analysis/source/prepare_data/create_data.r
+++ analysis/source/prepare_data/create_data.r
@@ -1,11 +1,9 @@
 library(yaml)
 CONFIG <- yaml.load_file("config_global.yaml")
-build_path <- CONFIG$build$prepare_data
-
 
 main <- function() {
     x <- 1:300000
-    write.table(x, sprintf("%s/data.txt", build_path),
+    write.table(x, sprintf("%s/data.txt", CONFIG$build$prepare_data),
                 row.names = FALSE, col.names = TRUE, quote = FALSE)
 }
 main()
